f8652fae79e48426cc719d20ef2d3ef5b7c2e863
Oleg Malichenko <omalichenko@magecore.com>
2016-11-29 15:00:45 +0200

    add chatbot

--- answer/Resolver.py
+++ answer/Resolver.py
@@ -1,6 +1,6 @@
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
-
+from chatterbot import ChatBot
 
 class Model(object):
     def __init__(self, updater):
@@ -8,6 +8,8 @@ class Model(object):
         self.data = []
         self.startPolling()
         self.bindEvents()
+        self.chatbot = ChatBot('Familiaris Alumnus', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+
 
     def bindEvents(self):
         self.dispatcher().add_handler(CommandHandler('start', self.start))
@@ -23,4 +25,4 @@ class Model(object):
         bot.sendMessage(chat_id=update.message.chat_id, text="Hello %s" % update.message.from_user.first_name)
 
     def echo(self, bot, update):
-        bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+        bot.sendMessage(chat_id=update.message.chat_id, text=self.chatbot.get_response(update.message.text))
